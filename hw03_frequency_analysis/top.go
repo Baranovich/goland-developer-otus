d4945b8275742bf1ed7f9b75f02e0997d9a962d4
ib <ip-oe@31-16-baranovskiy.ru>
2024-10-01 19:12:44 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,72 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var punctuationMarks = regexp.MustCompile(`[[:punct:]]`)
+
+func Top10(wordSequence string) []string {
+	var result []string
+
+	wordSequence = strings.ToLower(wordSequence)
+	wordArray := strings.Fields(wordSequence)
+
+	wordMap := make(map[string]int)
+
+	for _, word := range wordArray {
+		sequenceRunes := []rune(word)
+
+		transformedWord := proceedCharacter(sequenceRunes)
+
+		if len(transformedWord) != 0 {
+			if _, ok := wordMap[transformedWord]; ok {
+				wordMap[transformedWord]++
+			} else {
+				wordMap[transformedWord] = 1
+				result = append(result, transformedWord)
+			}
+		}
+	}
+
+	result = customSort(result, wordMap)
+
+	if len(result) > 10 {
+		return result[:10]
+	}
+
+	return result
+}
+
+func proceedCharacter(r []rune) string {
+	transformedWord := ""
+
+	for i := 0; i < len(r); i++ {
+		if punctuationMarks.MatchString(string(r[i])) {
+			if len(r) != 1 && !((i == 0 && r[i] != r[i+1]) || (i == len(r)-1 && r[i] != r[i-1])) {
+				transformedWord += string(r[i])
+			}
+		} else {
+			transformedWord += string(r[i])
+		}
+	}
+
+	return transformedWord
+}
+
+func customSort(result []string, wordMap map[string]int) []string {
+	sort.Slice(result, func(i int, j int) bool {
+		if wordMap[result[i]] > wordMap[result[j]] {
+			return true
+		} else if wordMap[result[i]] == wordMap[result[j]] {
+			if result[i] < result[j] {
+				return true
+			}
+		}
+		return false
+	})
+
+	return result
 }
--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -3,11 +3,12 @@ package hw03frequencyanalysis
 import (
 	"testing"
 
+	//nolint:depguard
 	"github.com/stretchr/testify/require"
 )
 
 // Change to true if needed.
-var taskWithAsteriskIsCompleted = false
+var taskWithAsteriskIsCompleted = true
 
 var text = `Как видите, он  спускается  по  лестнице  вслед  за  своим
 	другом   Кристофером   Робином,   головой   вниз,  пересчитывая
@@ -43,6 +44,10 @@ var text = `Как видите, он  спускается  по  лестни�
 	посидеть у огня и послушать какую-нибудь интересную сказку.
 		В этот вечер...`
 
+var textWithExtraData = `----, ---- .... '''' ,,,,, невероятно,,
+	но факт; ситуация оставляла желать худшего... .... так 
+	и запишем---- ----`
+
 func TestTop10(t *testing.T) {
 	t.Run("no words in empty string", func(t *testing.T) {
 		require.Len(t, Top10(""), 0)
@@ -77,6 +82,38 @@ func TestTop10(t *testing.T) {
 				"то",        // 4
 			}
 			require.Equal(t, expected, Top10(text))
+		}
+	})
+
+	t.Run("extra test", func(t *testing.T) {
+		if taskWithAsteriskIsCompleted {
+			expected := []string{
+				"----",
+				"....",
+				"''''",
+				",,,,,",
+				"желать",
+				"запишем----",
+				"и",
+				"невероятно,,",
+				"но",
+				"оставляла",
+			}
+			require.Equal(t, expected, Top10(textWithExtraData))
+		} else {
+			expected := []string{
+				"желать",
+				"запишем",
+				"и",
+				"невероятно",
+				"но",
+				"оставляла",
+				"ситуация",
+				"худшего",
+				"так",
+				"факт",
+			}
+			require.Equal(t, expected, Top10(textWithExtraData))
 		}
 	})
 }
